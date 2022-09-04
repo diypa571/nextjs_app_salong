2ae13ac8b66adb518b6377e72e95ef0cf3d74438
Diyar Parwana <diypa571@gmail.com>
2022-09-04 19:44:32 +0200

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+(function ($) {
+    "use strict";
+ 
+    $(".testimonial-carousel").owlCarousel({
+        nav: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        navText : [
+            '<i class="bi bi-chevron-left"> </i>',
+            '<i class="bi bi-chevron-right"> </i>'
+        ]
+    });
+
+
+})(jQuery);
